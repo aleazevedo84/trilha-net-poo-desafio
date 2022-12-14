f8d8e2a981ebfafcffd3da2cd239e7a7a2aed6ca
Alessandro Azevedo <aleazevedo84@outlook.com>
2022-12-14 17:44:13 -0300

    Finalizado classes conforme o diagrama

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone Iphone = new Iphone("1234", "modelo1", "1234", 64);
+Nokia Nokia = new Nokia("4321", "modelo1", "4321", 32);
+
+Nokia.Ligar();
+Iphone.ReceberLigacao();
+
+Nokia.InstalarAplicativo("WhatsApp");
+Iphone.InstalarAplicativo("Telegram");
